45e2e155852c5a7a3afc3116335231c9db3d60bf
DanielGG1024 <a22472510@gmail.com>
2021-07-10 21:03:05 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
 const app = express()
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -19,7 +20,8 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     Todo.find()
@@ -60,7 +62,7 @@ app.get('/todos/:id/edit', (req, res) => {
         .catch(error => console.log(error))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
     const id = req.params.id
     const { name, isDone } = req.body
     // const name = req.body.name
@@ -74,7 +76,7 @@ app.post('/todos/:id/edit', (req, res) => {
         .then(() => res.redirect(`/todos/${id}`))
         .catch(error => console.log(error))
 })
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
     const id = req.params.id
     return Todo.findById(id)
         .then(todo => todo.remove())
