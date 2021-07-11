7917261d9769fb1ea53cf13b032c09f40bd4bf52
DanielGG1024 <a22472510@gmail.com>
2021-07-11 20:34:17 +0800

    feat: add bootstrap & modify main.hbs

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 
 const express = require('express')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
 const routes = require('./routes')
 const methodOverride = require('method-override')
-const bodyParser = require('body-parser')
 
 require('./config/mongoose')
 const app = express()
-
-
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -21,3 +17,4 @@ app.use(routes)
 app.listen(3000, () => {
     console.log('this server is running on http"//localhost:3000')
 })
+
